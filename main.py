dbad14696eb26f46e04a79dfc96cec80fc8d82df
LaurentOngaro <laurent@gameamea.com>
2023-05-30 11:51:54 +0200

    feat: add buttons to save and clean result text

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ The main UETools window.
 """
 import tkinter as tk
 from tkinter import ttk
+from tkinter.filedialog import asksaveasfilename
+from tkinter.messagebox import showinfo
 
 from modules.functions import make_modal
 from modules.PluginVersionFixerClass import PluginsBuildIdFixer
@@ -17,11 +19,16 @@ class UETools(tk.Tk):
     def __init__(self):
         super().__init__()
         self.text_content = None
-        self.title('UE Tools')
-        self.geometry('500x500')
-        self.resizable(False, False)
-        self.create_widgets()
         self.keep_existing = False
+        self.width = 500
+        self.height = 270
+        self.file_types = (('csv file', '*.csv'), ('tcsv file', '*.tcsv'), ('json file', '*.json'), ('text file', '*.txt'))
+
+        self.title('UE Tools')
+        self.resizable(False, False)
+        self.geometry(f'{self.width}x{self.height}')
+
+        self.create_widgets()
         self.bind('<Key>', self.on_key)
         self.protocol("WM_DELETE_WINDOW", self.on_close)
 
@@ -30,28 +37,29 @@ class UETools(tk.Tk):
         Create the widgets for the UETools window.
         """
         pack_def_options = {'ipadx': 5, 'ipady': 5, 'padx': 3, 'pady': 3}
-        frm_top = tk.LabelFrame(self, text='Run Commands')
-        frm_center = tk.LabelFrame(self, text='Results')
-        frm_bottom = tk.LabelFrame(self)
+        lblf_top = tk.LabelFrame(self, text='Run Commands')
+        lblf_center = tk.LabelFrame(self, text='Results')
+        lblf_bottom = tk.LabelFrame(self)
 
-        frm_top.pack(side=tk.TOP, fill=tk.X, **pack_def_options)
-        frm_center.pack(fill=tk.X, **pack_def_options)
-        frm_bottom.pack(side=tk.BOTTOM, fill=tk.X, **pack_def_options)
+        lblf_top.pack(side=tk.TOP, fill=tk.X, **pack_def_options)
+        lblf_center.pack(fill=tk.X, **pack_def_options)
+        lblf_bottom.pack(side=tk.BOTTOM, fill=tk.X, **pack_def_options)
 
-        btn_update = ttk.Button(frm_top, text='Fix BuildId in Plugins', command=self.run_plugins_fix_build_id)
+        btn_update = ttk.Button(lblf_top, text='Fix BuildId in Plugins', command=self.run_plugins_fix_build_id)
         btn_update.pack(side=tk.LEFT, **pack_def_options)
 
         pack_def_options = {'ipadx': 3, 'ipady': 3}
-        text_content = tk.Text(frm_center, font=('Verdana', 8))
-        scrollbar_y = ttk.Scrollbar(frm_center)
+        text_content = tk.Text(lblf_center, font=('Verdana', 8))
+        scrollbar_y = ttk.Scrollbar(lblf_center)
         scrollbar_y.config(command=text_content.yview)
         text_content.config(yscrollcommand=scrollbar_y.set)
         scrollbar_y.pack(side=tk.RIGHT, fill=tk.Y, **pack_def_options)
         text_content.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, **pack_def_options)
 
         pack_def_options = {'ipadx': 5, 'ipady': 5, 'padx': 3, 'pady': 3}
-        btn_close = ttk.Button(frm_bottom, text='Close', command=self.close_app)
-        btn_close.pack(side=tk.RIGHT, **pack_def_options)
+        ttk.Button(lblf_bottom, text='Clean content', command=self.clean).pack(**pack_def_options, side=tk.LEFT)
+        ttk.Button(lblf_bottom, text='Save To File', command=self.save_to_file).pack(**pack_def_options, side=tk.LEFT)
+        ttk.Button(lblf_bottom, text='Close', command=self.close_app).pack(**pack_def_options, side=tk.RIGHT)
 
         self.text_content = text_content
 
@@ -91,6 +99,23 @@ class UETools(tk.Tk):
             self.text_content.insert(tk.END, content)
         # set the mode at the end to allow using display() to be used to change the mode for the next call
         self.keep_existing = keep_mode
+
+    def clean(self) -> None:
+        """
+        Clean the content of the window
+        """
+        self.text_content.delete('1.0', tk.END)
+
+    def save_to_file(self) -> str:
+        """
+        Save the content displayed to a file
+        """
+        filename = asksaveasfilename(title='Choose a file to save text to', filetypes=self.file_types, initialfile='results.txt')
+        if filename:
+            with open(filename, 'w') as f:
+                f.write(self.text_content.get('1.0', tk.END))
+            showinfo(self.wm_title(), f'Content Saved to {filename}')
+        return filename
 
     def run_plugins_fix_build_id(self) -> None:
         """
