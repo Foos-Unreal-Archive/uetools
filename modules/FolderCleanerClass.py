84c0ec2e7f6455cb77cc5da9d6cfb1df7d87040e
LaurentOngaro <laurent@gameamea.com>
2023-05-30 18:45:21 +0200

    feat: add a treeview to select the folders to delete

--- modules/FolderCleanerClass.py
+++ modules/FolderCleanerClass.py
@@ -7,6 +7,13 @@ import os
 import shutil
 import tkinter as tk
 from tkinter import ttk, messagebox as messagebox
+# https://ttkwidgets.readthedocs.io/en/sphinx_doc/ttkwidgets
+import ttkwidgets as ttkw
+
+# next import adds a hook to allow the -tooltips options to be added to tk widgets.
+# it's not correctly detected by pycharm, but it works.
+# noinspection PyUnresolvedReferences
+from ttkwidgets import tooltips
 
 from modules.ToolConfigClass import ToolConfig
 from modules.functions import browse_folder
@@ -25,20 +32,20 @@ class FolderCleaner(tk.Toplevel):
         self.name = 'FolderCleaner'
         self.description = 'Clean UE projects from build and intermediate folders.'
         self.width = 500
-        self.height = 270
+        self.height = 600
         self.config_file, self.config = self.init_config(self.name)
         self.build_id = ''
         self.result = f'\n###########\nRUNNING {self.name}\n###########\n'
         self.display_callback = display_callback
         self.error_list = []
-        self.folder_list = []
+        self.folder_list = []  # List of ALL the folders that have been found
 
         self.title('Projects Cleaner')
         self.resizable(False, False)
         self.geometry(f'{self.width}x{self.height}')
 
-        # Initialize StringVar variables for managing Entry widgets
         self.btn_execute = None
+        self.content_tree = None  # The treeview widget, with the list of SELECTED folders
         self.projects_folder_var = tk.StringVar()
         self.projects_folder_var.trace_add("write", lambda *args: self.config.set('projects_folder', self.projects_folder_var.get()))
         self.create_widgets()
@@ -66,12 +73,24 @@ class FolderCleaner(tk.Toplevel):
         """
         pack_def_options = {'ipadx': 5, 'ipady': 5, 'padx': 3, 'pady': 3}
         lbl_description = ttk.Label(self, text=self.description, wraplength=int(self.width * .9), font='TkDefaultFont 9 bold')
-        lblf_top = tk.LabelFrame(self, text='Folder that contains projects to clean')
-        lblf_bottom = tk.LabelFrame(self, text='Commands')
-
-
         lbl_description.pack(fill=tk.X, **pack_def_options)
+
+        lblf_top = tk.LabelFrame(self, text='Folder that contains projects to clean')
         lblf_top.pack(fill=tk.X, **pack_def_options)
+
+        # Folder list frame
+        lblf_content = ttk.LabelFrame(self, text='List of folders to clean')
+        lblf_content.pack(fill=tk.X, **pack_def_options)
+        content_tree = ttkw.CheckboxTreeview(lblf_content, selectmode='extended', columns='Folder')
+        content_tree.column('#0', width=30, stretch=tk.NO)
+        content_tree.heading('#0', text='Ck', anchor=tk.W)
+        content_tree.heading('Folder', text='Subfolder', anchor=tk.CENTER)
+        scrollbar = ttkw.AutoHideScrollbar(lblf_content, command=content_tree.yview)
+        content_tree.configure(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        content_tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.content_tree = content_tree
+        lblf_bottom = tk.LabelFrame(self, text='Commands')
         lblf_bottom.pack(fill=tk.X, **pack_def_options)
 
         # noinspection DuplicatedCode
@@ -105,26 +124,46 @@ class FolderCleaner(tk.Toplevel):
         Recursively find all the folder to clean from a given directory.
         :return: A list of folder paths.
         """
-        name_to_clean = ['Binaries', 'Build', 'DerivedDataCache', 'Intermediate']
+        names_to_clean = ['Binaries', 'Build', 'DerivedDataCache', 'Intermediate']
         folder_list = []
+        self.content_tree.delete(*self.content_tree.get_children())
         for root, dirs, files in os.walk(self.config.get('projects_folder')):
-            for name in name_to_clean:
-                if name in dirs:
-                    folder_list.append(os.path.join(root, name))
+            for to_clean in names_to_clean:
+                for dir_name in dirs:
+                    if to_clean == dir_name:
+                        path = os.path.join(root, dir_name)
+                        path = os.path.normpath(path)
+                        text = path.replace('\\', '/').replace(
+                            " ", "\\ "
+                        )  # escape spaces and backslashes bnecause they can't be displayed in the treeview
+                        folder_list.append(path)
+                        # values field will be displayed in the treeview
+                        # text field are used to retrieve the path when the folder is selected
+                        self.content_tree.insert('', 'end', text=path, values=text, tags='checked')
         return folder_list
 
-    def _clean_folders(self, folder_list: []) -> None:
+    def _clean_folders(self) -> None:
         """
         Clean all the folders in the list.
-        :param folder_list: A list of folder paths.
         """
-        if len(folder_list) == 0:
-            self.result += 'No folders found to clean.'
+
+        # get selected folders
+        selected_indexes = self.content_tree.get_checked()
+        if len(selected_indexes) == 0:
+            self.result += 'No folder to clean has been selected.'
             return
+        folder_list = []
+        for index in selected_indexes:
+            # path = self.content_tree.item(index)['values'][0]
+            path = self.content_tree.item(index)['text']
+            folder_list.append(path)
+
         for folder in folder_list:
             try:
-                shutil.rmtree(folder)
-                self.result += f'Cleaned {folder}\n'
+                # check if the folder is still there, it could have been deleted when its parent was deleted
+                if os.path.isdir(folder):
+                    shutil.rmtree(folder)
+                    self.result += f'Cleaned {folder}\n'
             except Exception as error:
                 self.result += f'Failed to clean {folder}: error {error!r}\n'
 
@@ -168,10 +207,10 @@ class FolderCleaner(tk.Toplevel):
 
         self.folder_list = self._find_folders()
         count = len(self.folder_list)
-        messagebox.showinfo('Command Result', f'Found {count} Folders to clean.')
         if count > 0:
             self.btn_execute.config(state=tk.NORMAL)
         else:
+            messagebox.showinfo('Command Result', f'No folder to clean has been found.')
             self.btn_execute.config(state=tk.DISABLED)
 
     def execute(self) -> None:
@@ -182,7 +221,7 @@ class FolderCleaner(tk.Toplevel):
             messagebox.showerror('Error', 'The list of project to clean is empty.')
             return
 
-        self._clean_folders(self.folder_list)
+        self._clean_folders()
         messagebox.showinfo('Command Result', 'Folder cleaned successfully.')
 
         self.config.save()
